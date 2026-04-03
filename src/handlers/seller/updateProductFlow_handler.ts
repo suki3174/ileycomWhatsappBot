06d25fd40b4f44ee7cd7384226260fbbdee0f160
souguir mouhamed taher <mouhamed.taher.souguir.work@gmail.com>
2026-04-03 05:58:17 +0100

    pushed more changes to update product catégory fixing

--- src/handlers/seller/updateProductFlow_handler.ts
+++ src/handlers/seller/updateProductFlow_handler.ts
@@ -1,5 +1,4 @@
-﻿﻿/* eslint-disable @typescript-eslint/no-explicit-any */
-import type { FlowRequest } from "@/models/flowRequest";
+﻿import type { FlowRequest } from "@/models/flowRequest";
 import type { FlowResponse } from "@/models/flowResponse";
 import {
   getFlowToken,
